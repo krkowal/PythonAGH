dd74f1c7908d8f396e3c44625a6911508724f440
krkowal <krzysztof.kowalik013@gmail.com>
2023-04-18 00:51:53 +0200

    added contexts' loops

--- zad1/System.py
+++ zad1/System.py
@@ -8,6 +8,13 @@ from zad1.Reader import Reader
 
 
 class System:
+    def __init__(self):
+        self.STANDARD_CONTEXT = {
+            "Borrow book": self.borrow_book_loop,
+            "Reserve book": self.reserve_book_loop,
+            "Prolong book rental": self.prolong_book_rental,
+            "View catalog": self.view_catalog
+        }
 
     def login(self):
         print("Welcome to our library!")
@@ -21,19 +28,25 @@ class System:
 
     def reader_context(self, user):
         print("reader context")
+        while True:
+            choice: str = self.show_context(list(self.STANDARD_CONTEXT.keys()))
+
+            self.STANDARD_CONTEXT[choice]()
 
     def librarian_context(self, user):
         print("librarian context")
 
-    def show_context(self, args):
+    def show_context(self, args, rtn=False):
         args_with_quit = deepcopy(args)
         args_with_quit.append("Quit")
+        if rtn:
+            args_with_quit.insert(len(args_with_quit) - 1, "Return")
         for i, line in enumerate(args_with_quit):
             print(f"{i + 1}. {line}")
         while True:
             try:
                 choice = int(input("Choose option: "))
-                if choice == len(args) + 1:
+                if choice == len(args_with_quit):
                     raise QuitError("User used quit")
                 return args_with_quit[choice - 1]
             except IndexError:
@@ -54,3 +67,15 @@ class System:
                     if user["email"] == email and user["password"] == password:
                         return user
             print("Email or password incorrect!")
+
+    def borrow_book_loop(self):
+        print("Borrow book loop")
+
+    def reserve_book_loop(self):
+        print("reserve_book_loop")
+
+    def prolong_book_rental(self):
+        print("prolong book rental loop")
+
+    def view_catalog(self):
+        print("view catalog loop")
