781f3bced90708d4b163d4c59c89967a89a2d027
krkowal <krzysztof.kowalik013@gmail.com>
2023-04-19 18:22:49 +0200

    added turning in books

--- zad1/Librarian.py
+++ zad1/Librarian.py
@@ -21,7 +21,44 @@ class Librarian(User):
         return f"Librarian\nEmail: {self.email}\nPassword: {self.password}"
 
     def take_in_return(self):
-        print("take in book return loop")
+        try:
+            book_id = int(input("Enter book's id: "))
+            with open(self.BOOKS_DB, 'r+') as bf:
+                with open(self.READER_DB, 'r+') as rf:
+                    books_data = json.load(bf)
+                    reader_data = json.load(rf)
+                    book = {}
+                    for b in books_data["books"]:
+                        if b["id"] == book_id:
+                            book = b
+                    if book is {}:
+                        print("Book no longer exists!")
+                        return False
+                    user, user_book = "", {}
+                    for u in reader_data["users"]:
+                        for b in reader_data["users"][u]["borrowed_books"]:
+                            if b["id"] == book_id:
+                                print("inside")
+                                user, user_book = u, b
+                    if user == "" or user_book is {}:
+                        print("This book is not borrowed")
+                        return False
+                    book.update({
+                        "borrowed_by": "",
+                        "borrowed_until": ""
+                    })
+                    print(user_book)
+                    reader_data["users"][user]["borrowed_books"].remove(user_book)
+                    bf.seek(0)
+                    bf.write(json.dumps(books_data, indent=4))
+                    bf.truncate()
+                    rf.seek(0)
+                    rf.write(json.dumps(reader_data, indent=4))
+                    rf.truncate()
+                    print("Book was successfully returned")
+                    return True
+        except ValueError:
+            print("Id should be a number!")
 
     def add_new_book(self):
         book = {}
