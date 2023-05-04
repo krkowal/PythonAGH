16fafa7684902f587deec3f08edc4c18206a2d7b
krkowal <krzysztof.kowalik013@gmail.com>
2023-05-05 00:20:01 +0200

    added word cleaning

--- zad2/main.py
+++ zad2/main.py
@@ -1,3 +1,6 @@
+import re
+
+
 def lines_generator(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
@@ -9,15 +12,17 @@ def count_words(file_path):
     gen = lines_generator(file_path)
     words_count = {}
     for line in gen:
+        pattern = r"""^\W|\W$"""
         words = line.strip().split()
-        print(words)
-        for word in words:
+        clean_words = list(map(lambda x: re.sub(pattern, "", x).lower(), words))
+        for word in clean_words:
             if words is not []:
                 if words_count.get(word) is not None:
                     words_count[word] = words_count[word] + 1
                 else:
                     words_count[word] = 1
+    return dict(sorted(words_count.items(), key=lambda x: x[1], reverse=True))
 
 
 if __name__ == '__main__':
-    print(count_words('potop.txt'))
+    print(count_words('potop_sample.txt'))
