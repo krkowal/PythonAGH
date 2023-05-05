d98b61c1e3c3375c7d8c6ec3e2194a19e216861b
krkowal <krzysztof.kowalik013@gmail.com>
2023-05-05 14:36:25 +0200

    zad2.2 complete

--- zad2/main.py
+++ zad2/main.py
@@ -8,7 +8,7 @@ def lines_generator(file_path):
             yield line
 
 
-def clean_line(line):
+def clean_line(line: str) -> list[str]:
     pattern = r"""[^\w\s]"""
     words = re.sub(pattern, "", line.strip())
     words = re.sub(r"\s{2,}", " ", words).lower().split()
@@ -29,11 +29,11 @@ def count_words(file_path) -> list[tuple[str, int]]:
     return sorted(words_count.items(), key=lambda x: x[1], reverse=True)
 
 
-def get_items_from_ordered_list(words_dict: list[tuple[str, int]], num: int):
+def get_items_from_ordered_list(words_list: list[tuple[str, int]], num: int) -> list[tuple[str, int]]:
     try:
-        word_count = words_dict[num - 1][1]
+        word_count = words_list[num - 1][1]
         lst = []
-        for item in words_dict:
+        for item in words_list:
             if item[1] >= word_count:
                 lst.append(item)
             else:
@@ -43,7 +43,7 @@ def get_items_from_ordered_list(words_dict: list[tuple[str, int]], num: int):
         print("There are not that many different words in this book")
 
 
-def get_n_gram(file_path, n: int):
+def get_n_gram(file_path, n: int) -> list[tuple[str, int]]:
     gen = lines_generator(file_path)
     n_gram = {}
     current_line = []
@@ -66,9 +66,9 @@ if __name__ == '__main__':
     place_no = int(input("How many words do you wish to see?: "))
     count_words_list = count_words('potop.txt')
     words_lst = get_items_from_ordered_list(count_words_list, place_no)
-    print(f"Words count: {words_lst}")
+    print(f"Top {place_no} word counts: {words_lst}")
     print("Printing bigram and trigram")
-    bigram_no = int(input("How many words of bigram do you wish to see?: "))
+    bigram_no = int(input("How many words of the bigram do you wish to see?: "))
     print(f"Bigram: {get_items_from_ordered_list(get_n_gram('potop.txt', 2), bigram_no)}")
-    trigram_no = int(input("How many words of trigram do you wish to see?: "))
+    trigram_no = int(input("How many words of the trigram do you wish to see?: "))
     print(f"Trigram: {get_items_from_ordered_list(get_n_gram('potop.txt', 3), trigram_no)}")
