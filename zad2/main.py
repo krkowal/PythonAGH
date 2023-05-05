33e146cf53f054d13c9a42346ed3fc186b30d19e
krkowal <krzysztof.kowalik013@gmail.com>
2023-05-05 13:22:28 +0200

    zad2.1 complete

--- zad2/main.py
+++ zad2/main.py
@@ -1,4 +1,5 @@
 import re
+from typing import List
 
 
 def lines_generator(file_path):
@@ -8,21 +9,41 @@ def lines_generator(file_path):
             yield line
 
 
-def count_words(file_path):
+def count_words(file_path) -> list[str]:
     gen = lines_generator(file_path)
     words_count = {}
     for line in gen:
-        pattern = r"""^\W|\W$"""
-        words = line.strip().split()
-        clean_words = list(map(lambda x: re.sub(pattern, "", x).lower(), words))
-        for word in clean_words:
+        pattern = r"""[^\w\s]"""
+        words = re.sub(pattern, "", line.strip())
+        words = re.sub(r"\s{2,}", " ", words).lower().split()
+        for word in words:
             if words is not []:
                 if words_count.get(word) is not None:
                     words_count[word] = words_count[word] + 1
                 else:
                     words_count[word] = 1
-    return dict(sorted(words_count.items(), key=lambda x: x[1], reverse=True))
+    return sorted(words_count.items(), key=lambda x: x[1], reverse=True)
+
+
+def get_items_from_ordered_list(words_dict: list[tuple[str, int]], num: int):
+    try:
+        word_count = words_dict[num - 1][1]
+        lst = []
+        for item in words_dict:
+            if item[1] >= word_count:
+                lst.append(item)
+            else:
+                break
+        return lst
+    except IndexError as err:
+        print("There are not that many different words in this book")
+
+
+# def get_n_gram(n:int):
 
 
 if __name__ == '__main__':
-    print(count_words('potop_sample.txt'))
+    place_no = int(input("How many words do you wish to see?: "))
+    count_words_list = count_words('potop.txt')
+    lst = get_items_from_ordered_list(count_words_list, place_no)
+    print(lst)
